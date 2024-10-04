800e0be1288f251eb64ccfa38a4745addc17e971
Bernardo Haab <57276499+BernardoHaab@users.noreply.github.com>
2024-10-04 20:59:50 +0000

    create syscall

--- board/qemu/x86/post-build.sh
+++ board/qemu/x86/post-build.sh
@@ -9,3 +9,8 @@ if [ -e ${TARGET_DIR}/etc/inittab ]; then
 	sed -i '/GENERIC_SERIAL/a\
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # QEMU graphical window' ${TARGET_DIR}/etc/inittab
 fi
+
+#Compile the syscall_test.c
+BUILDROOT_DIR=$BASE_DIR/..
+COMPILER=$BUILDROOT_DIR/output/host/bin/i686-buildroot-linux-gnu-gcc
+$COMPILER -o $BUILDROOT_DIR/output/target/bin/syscall_test $BUILDROOT_DIR/custom-scripts/syscall_test.c
