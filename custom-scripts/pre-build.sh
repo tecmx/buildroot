66ecd4819744837b0a841f02eb72780253440af3
Bernardo Haab <57276499+BernardoHaab@users.noreply.github.com>
2024-10-16 20:47:20 +0000

    feat: new disk

--- custom-scripts/pre-build.sh
+++ custom-scripts/pre-build.sh
@@ -4,9 +4,10 @@ cp $BASE_DIR/../custom-scripts/S41network-config $BASE_DIR/target/etc/init.d
 chmod +x $BASE_DIR/target/etc/init.d/S41network-config
 
 cp $BASE_DIR/../custom-scripts/hello/hello $BASE_DIR/target/usr/bin
-chmod +x $BASE_DIR/target/usr/bin
 
 cp $BASE_DIR/../T1-LabSisOp/t1-server $BASE_DIR/target/usr/bin
+
+cp $BASE_DIR/../disk-test/test_1 $BASE_DIR/target/usr/bin
 chmod +x $BASE_DIR/target/usr/bin
 
 make -C $BASE_DIR/../modules/simple_driver/
--- qemu-up.sh
+++ qemu-up.sh
@@ -3,5 +3,5 @@
 qemu-system-i386 --device e1000,netdev=eth0,mac=aa:bb:cc:dd:ee:ff \
 	--netdev tap,id=eth0,script=custom-scripts/qemu-ifup \
 	--kernel output/images/bzImage \
-	--hda output/images/rootfs.ext2 --nographic \
+	--hda output/images/rootfs.ext2 --hdb sdb.bin --nographic \
 	--append "console=ttyS0 root=/dev/sda"
